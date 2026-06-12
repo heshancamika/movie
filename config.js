832f81e251419160ac8e4ac9a53072522f2c4b7a
heshancamika <heshancamika@gmail.com>
2026-06-13 03:09:38 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    PORT: process.env.PORT || 3000,
+    SITE_URL: 'https://sinhalasub.lk',
+    
+    // 🍪 ඔයා එවපු කුකීස් ලිස්ට් එක Axios වලට කෙලින්ම යවන්න සකස් කළ ස්ට්‍රින් එක
+    COOKIE: '_ga_03W6RSCJV1=GS2.1.s1781299931$o1$g0$t1781299931$j60$l0$h0; s9ifs0idfjlwfie32dekl=0; _ga=GA1.1.737003549.1781299931; dom3ic8zudi28v8lr6fgphwffqoz0j6c=aba90e77-bf17-4e40-bd11-7d0858e37af7%3A2%3A1; hu8935j4i9fq3hpuj9q39=true; pp_idelay_1a10afe5fd8dc8069939f8a49fccbc26=1; starstruck_8c9b99985687fb6ab1d030c04b088ebb=a3b712c759a7296ae8164b72710e3237;',
+    
+    USER_AGENT: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'
+};
